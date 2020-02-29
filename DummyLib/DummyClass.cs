8d64553b39b00469c202044bb338038af6f3c9a8
Julian <moshammer.ju@gmail.com>
2020-02-29 22:11:42 +0100

    init

---
+++ DummyLib/DummyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyLib
+{
+    public class DummyClass
+    {
+        public void fDummy() => Console.WriteLine("DummyClass::fDummy");
+
+        public string gDummy(int x)
+        {
+            Console.WriteLine($"DummyClass::gDummy({x})");
+            return 
+        }
+        
+    }
+}
---
+++ ReflectionTutorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.Load("DummyLib");
+            Type type = assembly.GetType("DummyLib.DummyClass");
+            object instance = Activator.CreateInstance(type);
+            //DummyLib.DummyClass instance = new DummyLib.Dummyclass();
+            //instance.fDummy();
+            MethodInfo methodInfo = type.GetMethod("fDummy");
+            methodInfo.Invoke(instance, null);
+            Console.ReadKey();
+
+
+            //var type = Assembly.Load("DummyLib").GetType("DummyLib.DummyClass");
+            //var instance = Activator.CreateInstance(type);
+            var result = type?.GetMethod("gDummy")?.Invoke(instance, new object[] { 666 });
+        }
+    }
+}
